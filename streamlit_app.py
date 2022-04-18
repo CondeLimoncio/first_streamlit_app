a2f40a8263ac1eab2fed2540c1b823e3eb03db04
CondeLimoncio <ignaciovillalonga10@gmail.com>
2022-04-18 15:41:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import streamlit
 streamlit.title('Badge II Snowflake')
 streamlit.text('Lets goo babyyyy!')
 streamlit.title('LIGHT WEIGHT')
+
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
